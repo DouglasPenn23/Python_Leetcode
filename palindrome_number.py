da92f17ff73a1bcb686314f91b235a3cf54ce3f1
DouglasPenn23 <Douglaspennington2013@gmail.com>
2023-03-22 10:09:59 -0400

    Redid Palindrome problem for two pointers

--- palindrome_number.py
+++ palindrome_number.py
@@ -1,1 +1,44 @@
 # Given an integer x, return true if x is a palindrome, and false otherwise.
+# class Solution:
+#     def isPalindrome(self, x: int) -> bool:
+#         if x < 0:
+#             return False
+#         rev = 0
+#         orig = x
+#         while x != 0:
+#             rev = rev * 10 + x % 10
+#             x //= 10
+#         return rev == orig
+
+# New solution that is more understandable for me
+# Convert to String
+# class Solution:
+#     def isPalindrome(self, x: int) -> bool:
+#         string = str(x)
+#         left = 0
+#         right = len(str) - 1
+
+#         while left < right:
+#             if string[left] != string[right]:
+#                 return False
+            
+#             left += 1
+#             right -= 1
+
+#             return True
+
+#  My personal solution
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        string = str(x)
+        left = 0
+        right = len(str) - 1
+
+        while left < right:
+            if string[left] != string[right]:
+                return False
+            
+            left += 1
+            right -= 1
+
+            return True
